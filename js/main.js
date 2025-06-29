15719ed5f21f54155288e09eb2366db1cd68ddc2
Marcelo Gelato <gelatomarcelo77@gmail.com>
2025-06-29 16:24:07 -0300

    finalizado

--- js/main.js
+++ js/main.js
@@ -28,9 +28,6 @@ function registroFormulario(e) {
     } else {
         aviso.innerHTML = "captcha incorrecto";
     }
-    // let usuario = document.querySelector("#usuario").value;
-    // let password = document.querySelector("#password").value;
-    // let captcha = document.querySelector("#captcha").value;
 }
 let form = document.querySelector("#form");
 if (form) {
@@ -53,11 +50,16 @@ cargarReferencia();
 const urlUsuario = "https://6845ab1afc51878754dbee55.mockapi.io/api/v1/usuarios";
 const formRegistro = document.querySelector("#formRegistro");
 let id = 0;
-let usuarioInput = "";
 
 async function obtenerUsuarios() {
     const lista = document.querySelector("#tabla_usuarios");
-    lista.innerHTML = "";
+    if (lista) {
+        lista.innerHTML = "";
+    } else {
+        console.log("No hay lista de usuario");
+        return;
+    }
+
     try {
         let res = await fetch(urlUsuario); //por defecto fetch hace GET al explorador
         let json = await res.json(); //texto json a objeto
@@ -84,11 +86,11 @@ async function obtenerUsuarios() {
         }
 
     } catch (error) {
-        console.log("Error");
+        console.log("Error", error);
     }
 }
 
-    ////////////////POST personas///////////////
+////////////////POST personas///////////////
 async function enviarUsuario(e) {
     e.preventDefault();
     let nombre = document.querySelector("#nombre").value;
@@ -100,6 +102,11 @@ async function enviarUsuario(e) {
 
     document.querySelector("#msg").innerHTML = "Usuario registrado!";
 
+    if (!nombre || !apellido || !usuario || !password) {
+        document.querySelector("#msg").innerHTML = "Ingrese los datos obligatorios";
+        return;
+    }
+
     let persona = {
         nombre: nombre,
         apellido: apellido,
@@ -108,23 +115,27 @@ async function enviarUsuario(e) {
         usuario: usuario,
         password: password
     }
-    
+
     try {
         let res = await fetch(urlUsuario, {
             "method": "POST",
-            "headers": {"Content-type": "application/json"},
+            "headers": { "Content-type": "application/json" },
             "body": JSON.stringify(persona)
         })
-        if(res.status == 201) {
-            console.log("usuario creado");
+        if (res.status === 201) {
+            console.log("usuario registrado");
             obtenerUsuarios();
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
     document.querySelector("#form-registro").reset();// resetea los datos ya ingresados en los input del form
 }
-document.querySelector("#btn-registro").addEventListener("click", enviarUsuario);
+
+let btnRegistro = document.querySelector("#btn-registro");
+if (btnRegistro) {
+    document.querySelector("#btn-registro").addEventListener("click", enviarUsuario);
+}
 
 /////////////PUT personas//////////////////////
 async function modificarUsuario(e) {
@@ -137,23 +148,23 @@ async function modificarUsuario(e) {
     document.querySelector("#form-registro").innerHTML = `
         <h4>Modificar usuario</h4>
         <form id="form-modificar">
-            <input type="text" id="nombre" value="${usuario.nombre}" placeholder="Nombre" required>
-            <input type="text" id="apellido" value="${usuario.apellido}" placeholder="Apellido" required>
+            <input type="text" id="nombre" value="${usuario.nombre}" placeholder="Nombre">
+            <input type="text" id="apellido" value="${usuario.apellido}" placeholder="Apellido">
             <input type="number" id="telefono" value="${usuario.telefono}" placeholder="Teléfono">
             <input type="text" id="ciudad" value="${usuario.ciudad}" placeholder="Ciudad">
-            <input type="text" id="usuario" value="${usuario.usuario}" placeholder="Usuario" required>
-            <input type="text" id="password" value="${usuario.password}" placeholder="Contraseña" required>
+            <input type="text" id="usuario" value="${usuario.usuario}" placeholder="Usuario">
+            <input type="text" id="password" value="${usuario.password}" placeholder="Contraseña">
             <button type="button" id="btn-guardar">Guardar cambios</button>
             <button type="button" id="btn-cancelar">Cancelar</button>
         </form>
     `;
 
-    document.querySelector("#btn-cancelar").addEventListener("click", function() {
+    document.querySelector("#btn-cancelar").addEventListener("click", function () {
         document.querySelector("#form-registro").innerHTML = formRegistroOriginal;
     })
-    document.querySelector("#btn-guardar").addEventListener("click", async function(e) {
-    e.preventDefault();
-        
+    document.querySelector("#btn-guardar").addEventListener("click", async function (e) {
+        e.preventDefault();
+
         let persona = {
             nombre: document.querySelector("#nombre").value,
             apellido: document.querySelector("#apellido").value,
@@ -164,7 +175,7 @@ async function modificarUsuario(e) {
         };
         let resPut = await fetch(`${urlUsuario}/${idModificar}`, {
             method: "PUT",
-            headers: {"Content-type": "application/json"},
+            headers: { "Content-type": "application/json" },
             body: JSON.stringify(persona)
         });
         if (resPut.status === 200) {
@@ -185,182 +196,49 @@ async function eliminarUsuario(e) {
         let resDelete = await fetch(`${urlUsuario}/${idEliminar}`, {
             "method": "DELETE"
         })
-        if(resDelete.status === 200) {
+        if (resDelete.status === 200) {
             document.querySelector("#msg").innerHTML = "Usuario eliminado!";
             obtenerUsuarios(); //refresca la lista
         } else {
             document.querySelector("#msg").innerHTML = "Usuario no encontrado";
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
 
 obtenerUsuarios();
 
+////////////////partial render////////////
+function asociarPartialRender() {
+    document.querySelectorAll(".link-bazar").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            partialRender("bazar.html");
+        });
+    });
+    document.querySelectorAll(".link-almacen").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            partialRender("almacen.html");
+        });
+    });
+}
 
+asociarPartialRender();
 
-
-//-----------------tabla productos-------------->
-///////////////GET productos//////////////////////
-// async function obtenerDatos() {
-//     const urlProductos = "https://6845ab1afc51878754dbee55.mockapi.io/api/v1/carrito";
-//     // e.preventDefault();
-//     const lista = document.querySelector("#listado");
-//     lista.innerHTML = "";
-//     try {
-//         let res = await fetch(urlProductos); // GET al listado y se obtienen datos de productos en la API
-//         let json = await res.json(); // así el texto json se parsea (compila) a un objeto
-//         console.log(json);
-//         for (const productos of json) {
-//             let cantidad = productos.cantidad;
-//             let producto = productos.producto;
-//             let precio = productos.precio;
-//             lista.innerHTML += `<tr>
-//                                 <td>${cantidad}</td>
-//                                 <td>${producto}</td>
-//                                 <td>${precio}</td>
-//                                 <td><button style="cursor:pointer">Modificar</button</td>
-//                                 <td><button style="cursor:pointer">Eliminar</button></td>
-//                                 </tr>`;
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// obtenerDatos();
-
-// ////////////////POST productos/////////////////
-// const urlProductos = "https://6845ab1afc51878754dbee55.mockapi.io/api/v1/carrito";
-// async function agregarProductos(e) {
-//     e.preventDefault();
-//     let cantidad = document.querySelector("#cantidad").value;
-//     let producto = document.querySelector("#producto").value;
-//     let precio = document.querySelector("#precio").value;
-
-//     contenedor.innerHTML = cantidad + producto + " se agregó al carrito";
-
-//     let nuevoProducto = {
-//         cantidad: cantidad,
-//         producto: producto,
-//         precio: precio
-//     };
-
-//     try {
-//         let res = await fetch(urlProductos, {
-//             method: "POST",
-//             headers: { "Content-type": "application/json" },
-//             body: JSON.stringify(nuevoProducto)
-//         })
-//         let json = await res.json();
-//         console.log(json);
-
-//         obtenerDatos();
-//     } catch (error) {
-//         console.log(error);
-
-//     }
-// }
-
-// let formAgregar = document.querySelector("#listado");
-// if (formAgregar) {
-//     formAgregar.addEventListener('submit', agregarProductos);
-// }
-
-// ////////////////POST usuarios/////////////////
-// const urlUsuario = "https://6845ab1afc51878754dbee55.mockapi.io/api/v1/usuarios";
-// const contenedor = document.querySelector("#registro");
-
-// async function registrarse(e) {
-//     e.preventDefault();
-//     let nombre = document.querySelector("#nombre").value;
-//     let apellido = document.querySelector("#apellido").value;
-//     let telefono = document.querySelector("#telefono").value;
-//     let ciudad = document.querySelector("#ciudad").value;
-//     let usuario = document.querySelector("#usuario").value;
-//     let password = document.querySelector("#password").value;
-
-//     contenedor.innerHTML = nombre + " " + apellido + " " + " ha quedado registrado/a!";
-
-//     let usuarios = {
-//         nombre: nombre,
-//         apellido: apellido,
-//         telefono: telefono,
-//         ciudad: ciudad,
-//         usuario: usuario,
-//         password: password
-//     };
-
-//     try {
-//         let res = await fetch(urlUsuario, {
-//             "method": "POST",
-//             "headers": { "Content-type": "application/json" },
-//             "body": JSON.stringify(usuarios)
-//         });
-//         let json = await res.json();
-//         console.log(json);
-//         let idUsuario = json.id;
-
-//         contenedor.innerHTML = "Tu ID de usuario es: " + idUsuario;
-
-//         if (res.status === 201)
-//             console.log(nombre + " " + apellido + " " + " ha quedado registrado/a!");
-//     } catch (error) {
-//         console.log(error);
-
-//     }
-// }
-
-// let formRegistro = document.querySelector("#form-registro");
-// if (formRegistro) {
-//     formRegistro.addEventListener('submit', registrarse);
-// }
-
-// //////////////obtener ID usuario////////////
-// async function obtenerIDporUsuario(usuario) {
-//     const respuesta = await fetch(`https://6845ab1afc51878754dbee55.mockapi.io/api/v1/usuarios?usuario=${usuario}`);
-//     if (!respuesta.ok) {
-//         alert("No se pudo obtener el ID del usuario");
-//     }
-//     let datos = await respuesta.json();
-//     return datos.id;
-// }
-
-// /////////////cambiar contraseña/////////////
-// async function cambiarContrasenia(e) {
-//     e.preventDefault();
-
-//     let newPass = document.querySelector("#newPass");
-//     newPass.innerHTML = `<h5>Usuario: <input type="text" placeholder="usuario"></h5>
-//                         <h5>Contraseña actual: <input type="text" placeholder="actual"></h5>
-//                         <h5>Nueva contraseña: <input type="text" placeholder="nueva"</h5>
-//                         <button id="btn-enviarPass">Enviar</button>`
-
-//     document.querySelector("#btn-enviarPass").addEventListener("click", async function () {
-//         let usuario = document.querySelector("#usuario").value;
-//         let actual = document.querySelector("#actualPass").value;
-//         let nueva = document.querySelector("#nuevoPass");
-//     })
-
-//     try {
-//         let res = await fetch(`https://6845ab1afc51878754dbee55.mockapi.io/api/v1/usuarios?usuario=${usuario}`);
-//         let data = await res.json();
-
-//         if (data.length > 0 && data[0].password === actual) {
-            
-//         }
-//     } catch (error) {
-
-//     }
-
-// }
-// document.querySelector("#btn-cambiar").addEventListener('click', cambiarContrasena);
-
-
-
-// //////////////PUT usuarios//////////////////
-// async function actualizar(e) {
-//     e.preventDefault();
-// }
-// document.querySelector("#btn-cambiar").addEventListener('click', actualizar);
+async function partialRender(ruta) {
+    try {
+        let res = await fetch(ruta);
+        let text = await res.text();
+        let temp = document.createElement('div');
+        temp.innerHTML = text;
+        let nuevoMain = temp.querySelector('main.content');
+        if (nuevoMain) {
+            document.querySelector('main.content').innerHTML = nuevoMain.innerHTML;
+            asociarPartialRender();
+        }
+    } catch (error) {
+        console.log('Error al cargar el contenido:', error);
+    }
+}
